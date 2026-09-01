9decd370b2d80861764a75d14321d7d4bbb26289
Manos Dramitinos <mdramit@gmail.com>
2026-09-01 23:12:31 +0300

    Arrays js example

---
+++ arrays.js
@@ -0,0 +1,8 @@
+const nums = [10, 20, 30];
+console.log(nums.length);         // 3
+console.log(nums[0]);             // 10
+nums.push(40);      // add to the end → [10,20,30,40]
+
+console.log(nums);
+const mixed = [1, "two", true, null];  
+console.log(mixed);
